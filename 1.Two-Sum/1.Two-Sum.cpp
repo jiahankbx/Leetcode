c1d0c6da00220b9ac9f387eae9e4c41b23198292
jiahankbx <1422048708@qq.com>
2016-11-06 17:34:34 +0800

    Create 1.Two-Sum.cpp

---
+++ 1.Two-Sum/1.Two-Sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+ public:
+     vector<int> twoSum(vector<int> &nums, int target) {
+     vector<int> result;
+         for (int i = 0; i < nums.size()-1; i++) {
+             for (int j = i+1; j < nums.size(); j++) {
+                 if (nums[i] + nums[j]==target) {
+                     result.push_back(i);
+                     result.push_back(j);
+                     return result;
+                 }
+             }
+         }
+         return result;
+     }
+ };
+ 
+ 689ms beat 10%
+ 
+ class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        int dummy;
+        for(int i=0;i<nums.size()-1;i++){
+            dummy=target-nums[i];
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[j]==dummy){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }    
+        }
+        return result;
+    }
+};
+
+492ms beat 15%
