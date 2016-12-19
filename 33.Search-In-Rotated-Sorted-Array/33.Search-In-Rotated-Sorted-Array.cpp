8aa2c3411ba024e4e10ba9112fb1f5f341c7d1c1
jiahankbx <1422048708@qq.com>
2016-12-19 20:52:34 +0800

    Create 33.Search-In-Rotated-Sorted-Array.cpp

---
+++ 33.Search-In-Rotated-Sorted-Array/33.Search-In-Rotated-Sorted-Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target) return i;
+            else if(nums[i]<target&&nums[i+1]>target) return -1;
+            else if(nums[i]<target&&nums[i]>nums[i+1]) return -1;
+            else if(nums[i]>target) continue;
+        }
+    return -1;
+    }
+};
+
+3ms 65.27%
