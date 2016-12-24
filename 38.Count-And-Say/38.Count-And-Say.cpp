f20fa6dcf1090b719f809607dd87680fad9a0e7a
jiahankbx <1422048708@qq.com>
2016-12-24 16:54:37 +0800

    Update 38.Count-And-Say.cpp

--- 38.Count-And-Say/38.Count-And-Say.cpp
+++ 38.Count-And-Say/38.Count-And-Say.cpp
@@ -2,21 +2,22 @@ class Solution {
 string cs(string str){
     string s="";
     int temp=1;
-    for(int j=0;j<str.size();j++){
+    for(int j=0;j<str.size();j++){ //j用来对前一个数的进行遍历
         if(str[j]!=str[j+1]) s=s+'1'+str[j];
         else if(str[j]==str[j+1]){
             temp++;
-            while(j+temp<str.size()&&str[j]==str[j+temp]){
+            while(j+temp<str.size()&&str[j]==str[j+temp]){ //temp用来记录有多少个连续的相同元素
                 temp++;
             }
-            string test=to_string(temp);
+            string test=to_string(temp); //to_string把temp转换为字符串
             s=s+test+str[j];
             j=j+temp-1;
             temp=1;
         }
     }
     return s;
-}    
+}   
+//程序优化空间很大
     
 public:
     string countAndSay(int n) {
@@ -24,7 +25,7 @@ public:
         string st="1";
         if(n<=0) return "";
         else if(n==1) return "1";
-        else{
+        else{ //i用来计算循环次数
             for(i=1;i<n;i++){st=cs(st);}
             return st;
         }
